b84b64998d88a1e6efc63058ccde8b51f064b883
fenyx4 <Fenyx4@gmail.com>
2020-07-28 22:52:54 -0500

    Favoring moving towards lower ground on the way to the water looks much better!

--- U4DosRandomizer/WorldMap.cs
+++ U4DosRandomizer/WorldMap.cs
@@ -11,15 +11,31 @@ namespace U4DosRandomizer
         private double[,] _worldMapGenerated;
         private byte[,] _worldMapTiles;
 
+        private double _generatedMin;
+        private double _generatedMax;
+
         public const int SIZE = 256; 
 
         public WorldMap(double[,] worldMapGenerated)
         {
             _worldMapGenerated = worldMapGenerated;
-            _worldMapTiles = MapGeneratedMapToUltimaTiles(_worldMapGenerated);
+            _worldMapTiles = MapGeneratedMapToUltimaTiles();
+
+            var worldMapFlattened = new double[WorldMap.SIZE * WorldMap.SIZE];
+
+            for (int x = 0; x < WorldMap.SIZE; x++)
+            {
+                for (int y = 0; y < WorldMap.SIZE; y++)
+                {
+                    worldMapFlattened[x + y * WorldMap.SIZE] = _worldMapGenerated[x, y];
+                }
+            }
+
+            _generatedMin = worldMapFlattened.Min();
+            _generatedMax = worldMapFlattened.Max();
         }
 
-        private static byte[,] MapGeneratedMapToUltimaTiles(double[,] worldMapDS)
+        private byte[,] MapGeneratedMapToUltimaTiles()
         {
             var worldMapFlattened = new double[WorldMap.SIZE * WorldMap.SIZE];
 
@@ -27,7 +43,7 @@ namespace U4DosRandomizer
             {
                 for (int y = 0; y < WorldMap.SIZE; y++)
                 {
-                    worldMapFlattened[x + y * WorldMap.SIZE] = worldMapDS[x, y];
+                    worldMapFlattened[x + y * WorldMap.SIZE] = _worldMapGenerated[x, y];
                 }
             }
 
@@ -61,8 +77,8 @@ namespace U4DosRandomizer
                     byte res = 99;
                     foreach (var range in rangeList)
                     {
-                        var value = worldMapDS[x, y];
-                        if (worldMapDS[x, y] > range.Item2 && worldMapDS[x, y] <= range.Item3)
+                        var value = _worldMapGenerated[x, y];
+                        if (_worldMapGenerated[x, y] > range.Item2 && _worldMapGenerated[x, y] <= range.Item3)
                         {
                             res = range.Item1;
                             break;
@@ -127,7 +143,7 @@ namespace U4DosRandomizer
         private void AddRivers(Random random)
         {
             // There are ~32 in the original Ultima map
-            int totalNumOfRivers = 7 + random.Next(1, 3) + random.Next(1, 3);
+            int totalNumOfRivers = 28 + random.Next(1, 3) + random.Next(1, 3);
             var surroundingPoints = new List<Point>();
 
             for (int dist = 1; dist <= 3; dist++)
@@ -154,7 +170,7 @@ namespace U4DosRandomizer
                 Coordinate foundHighPoint = null;
                 while (foundHighPoint == null)
                 {
-                    //worldMapUlt[prevPoint.X, prevPoint.Y] = 0xA1;
+                    //_worldMapTiles[prevPoint.X, prevPoint.Y] = 0xA1;
                     var highestDirection = surroundingPoints.OrderByDescending(p => _worldMapGenerated[Wrap(currPoint.X + (p.X)), Wrap(currPoint.Y + (p.Y))]).First();
 
                     if (_worldMapGenerated[currPoint.X, currPoint.Y] < _worldMapGenerated[Wrap(currPoint.X + (highestDirection.X)), Wrap(currPoint.Y + (highestDirection.Y))])
@@ -170,7 +186,7 @@ namespace U4DosRandomizer
                     }
                 }
 
-                //worldMapUlt[prevPoint.X, prevPoint.Y] = 76;
+                //_worldMapTiles[prevPoint.X, prevPoint.Y] = 76;
                 highPoints.Add(prevPoint);
             }
 
@@ -178,7 +194,7 @@ namespace U4DosRandomizer
             foreach (var highPoint in highPoints)
             {
                 // find shortest path
-                var path = Search.GetPath(WorldMap.SIZE, WorldMap.SIZE, new List<Coordinate> { highPoint }, IsCoordinateWater, delegate { return true; });
+                var path = Search.GetPath(WorldMap.SIZE, WorldMap.SIZE, new List<Coordinate> { highPoint }, IsCoordinateWater, delegate { return true; }, FindWaterHueristic);
                 paths.Add(path);
                 //List<Point> pathToWater = new List<Point>();
                 //pathToWater.Add(highPoint);
@@ -220,6 +236,10 @@ namespace U4DosRandomizer
                 var max = path.Count - 10;
                 var min = 10;
                 var start = Convert.ToInt32(Math.Floor(Math.Abs(random.NextDouble() - random.NextDouble()) * (1 + max - min) + min));
+                //for( int i = 1; i < start; i++)
+                //{
+                //    _worldMapTiles[path[i].X, path[i].Y] = 0x70;
+                //}
                 for (int i = start; i < path.Count; i++)
                 {
                     _worldMapTiles[path[i].X, path[i].Y] = 0x02;
@@ -229,9 +249,23 @@ namespace U4DosRandomizer
             return;
         }
 
+        //public delegate float NodeHuersticValue(Coordinate coord, IsNodeValid matchesGoal);
+        private float FindWaterHueristic(Coordinate coord, IsNodeValid matchesGoal)
+        {
+            if( matchesGoal(coord))
+            { 
+                return 1.0f; 
+            }
+
+            var range = _generatedMax - _generatedMin;
+            var value = _worldMapGenerated[coord.X, coord.Y] - _generatedMin;
+
+            return Convert.ToSingle(value / range);
+        }
+
         private static bool IsCoordinateWater(ICoordinate coordinate)
         {
-            return coordinate.GetTile() < 3;
+            return coordinate.GetTile() < 2;
         }
 
         private static Coordinate FindRandomPointHigherThan(int tile, byte[,] worldMapUlt, Random random)
