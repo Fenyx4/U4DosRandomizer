3d5f57dbc7846715adc6e2ba15b9d3bcc39edac9
Fenyx4 <Fenyx4@gmail.com>
2025-08-21 04:23:57 -0500

    MAUI initial commit

---
+++ U4DosRandomizer.UI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace U4DosRandomizer.UI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
